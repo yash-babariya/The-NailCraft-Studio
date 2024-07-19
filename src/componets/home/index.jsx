7c8ee8b5a382fff8906cb2ad587bad8839dec6ae
yash-babariya <yashbabariya549@gmail.com>
2024-07-19 16:11:43 +0530

    some changes

--- src/componets/home/index.jsx
+++ src/componets/home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>The NailCraft Studio</h1>
         </div>
     )
 }
